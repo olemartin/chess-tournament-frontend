8e2bf55d83d18122a069f0192e6446b37416e6c6
selbekk <kristofer.selbekk@bekk.no>
2016-12-15 23:42:01 +0100

    Add UI support for creating tournaments

--- src/containers/overview-page/OverviewPage.js
+++ src/containers/overview-page/OverviewPage.js
@@ -1,14 +1,54 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+import Dialog from 'material-ui/Dialog';
 import { List, ListItem } from 'material-ui/List';
 import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 import DeleteIcon from 'material-ui/svg-icons/action/delete';
 import CenteredWrapper from '~/components/centered-wrapper';
 
 class OverviewPage extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            newTournamentDialogOpen: false,
+        };
+
+        this.onOpenDialog = this.onOpenDialog.bind(this);
+        this.onCreateNewTournament = this.onCreateNewTournament.bind(this);
+    }
+
+    onOpenDialog() {
+        this.setState({
+            newTournamentDialogOpen: true,
+        });
+    }
+
+    onCreateNewTournament() {
+        // TODO: Create a new tournament
+
+        this.setState({
+            newTournamentDialogOpen: false,
+        });
+    }
+
     render() {
-        const { tournaments } = this.props;
+        const {
+            tournaments,
+        } = this.props;
+
+        const {
+            newTournamentDialogOpen,
+        } = this.state;
+
+        const dialogActions = [
+            <RaisedButton
+                label="Create"
+                onTouchTap={this.onCreateNewTournament}
+            />,
+        ];
 
         return (
             <CenteredWrapper>
@@ -27,7 +67,15 @@ class OverviewPage extends Component {
                         )}
                     </List>
                 }
-                <RaisedButton label="New tournament" primary />
+                <RaisedButton
+                    label="New tournament"
+                    onTouchTap={this.onOpenDialog}
+                    primary
+                />
+                <Dialog open={newTournamentDialogOpen} actions={dialogActions}>
+                    <h2>Create a new tournament!</h2>
+                    <TextField floatingLabelText="Tournament name" />
+                </Dialog>
             </CenteredWrapper>
         );
     }
