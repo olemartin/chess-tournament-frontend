54bf79c35b7e4699c23ae0d05bea3547f49b0eca
selbekk <kristofer.selbekk@bekk.no>
2016-12-15 22:43:55 +0100

    Add Material-UI
    Sounds like a great time-saver

--- src/containers/root/Root.js
+++ src/containers/root/Root.js
@@ -1,11 +1,20 @@
 import React, { PropTypes } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 import App from '~/containers/app';
 
+const theme = getMuiTheme({
+    // TODO: Set up colors etc.
+    // http://www.material-ui.com/#/customization/themes
+});
+
 const RootContainer = props => (
-    <App>
-        {props.children}
-    </App>
+    <MuiThemeProvider muiTheme={theme}>
+        <App>
+            {props.children}
+        </App>
+    </MuiThemeProvider>
 );
 
 RootContainer.propTypes = {
--- src/index.js
+++ src/index.js
@@ -5,10 +5,15 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
+import injectTapEventPlugin from 'react-tap-event-plugin';
 import './index.less';
 
 import configureStore from './store/configure-store';
 import routes from './routes';
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
